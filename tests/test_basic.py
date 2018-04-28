455958c87bfc236513bf93dde525cfe0b665b947
Gabriel <gabriel59kg@gmail.com>
2018-04-28 13:58:48 -0400

    fix test paths

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,11 +6,13 @@ simple tests for bt_expense.
 import os
 import unittest
 import pytest
-
+from sys import version_info
 # import pytest
 
 from context import bt_expense as bte
 from context import fixpath
+
+PYTHON_VER = version_info[0]
 
 TEST_DIR = fixpath(os.path.abspath(os.path.dirname(__file__)))
 ROOT_DIR = fixpath(os.path.dirname(TEST_DIR))
@@ -22,7 +24,8 @@ def test_pulling_column_values():
         os.chdir(MAIN_DIR)
     except FileNotFoundError:
         pass
-    a1 = bte.get_values('Expenses', 'A1')[0]
+    a1 = bte.get_values('Expenses', 'A1',
+                        workbook_name='Expenses_Template.xlsx')[0]
     assert a1, 'Project'
     os.chdir(ROOT_DIR)
 
@@ -38,10 +41,11 @@ class AuthorizerTests(unittest.TestCase):
         os.chdir(ROOT_DIR)
 
     def test_authorizer_object_creation(self):
-        bte.Authorizer('bt_expense/Expenses.xlsx')
+        bte.Authorizer('bt_expense/Expenses_Template.xlsx')
 
 
 if __name__ == "__main__":
+    print('Python {}'.format(PYTHON_VER))
     print(__doc__)
     print(__file__)
     print('root:', ROOT_DIR)
